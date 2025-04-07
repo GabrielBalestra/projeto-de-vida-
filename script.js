c3e37169acb988d8ab264561752b49518f91fb37
GabrielBalestra <146239343+GabrielBalestra@users.noreply.github.com>
2025-04-07 08:21:46 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
+const botoes = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
